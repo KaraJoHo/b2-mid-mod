555086119584524535b7c90cffd028736b5b3b74
KaraJH <karas.jones@yahoo.com>
2023-02-19 12:00:42 -0700

    test: Employee and department validations

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,3 +1,4 @@
 class Department < ApplicationRecord
   has_many :employees
+  validates_presence_of :name, :floor
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,3 +1,5 @@
 class Employee < ApplicationRecord
   belongs_to :department
+
+  validates_presence_of :name, :level
 end
